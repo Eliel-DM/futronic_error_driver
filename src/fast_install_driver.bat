49c0b4b047f12569810f7fb06707bcfec0d9bc82
Eliel-DM <elieldiasmatos@gmail.com>
2025-08-31 16:01:35 -0300

    Foi identificado o parâmetro para realizar a instalação do driver através do modo silencioso. 'nome do driver.exe' -silentinstall . Foi descoberto através do processo de análise das strings do executável através da ferramenta Detect It Easy

--- src/fast_install_driver.bat
+++ src/fast_install_driver.bat
@@ -1,1 +1,3 @@
+@echo off
+driver.exe -silentinstall
 :: Instalar rapidamento o driver para testes;
--- src/index.bat
+++ src/index.bat
@@ -1,3 +1,33 @@
 @echo off
-:: Apaga o registro do driver.
+setlocal
+
+:: Baixa o driver do repositório remoto no github.
+curl -L -o "driver.exe" "https://github.com/Eliel-DM/futronic_error_driver/raw/main/src/driver.exe"
+
+driver.exe -silentremove
+
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\FutronicDrv" /f
+
+:: Faz a instalação após reiniciar o pc 
+if "%1"=="after_reboot" (
+    echo Executando instalacao pos-reboot...
+    driver.exe -silentinstall
+    
+    :: Remove os arquivos baixados para a instalação.
+    del "driver.exe" /q
+    del "%~f0" /q
+    
+    msg * "Driver Futronic instalado com sucesso!"
+    
+    :: Remove a tarefa agendada
+    schtasks /delete /tn "InstalarFutronicDriver" /f
+    goto :fim
+)
+
+:: Agenda a tarefa para executar após o reboot
+schtasks /create /tn "InstalarFutronicDriver" /tr "\"%~f0\" after_reboot" /sc onlogon /ru SYSTEM /f
+
+shutdown /r /t 5 /c "Reiniciando para completar a instalacao do driver"
+
+:fim
+endlocal
